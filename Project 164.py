9df2460731d53e87f01b40cc4f43079fe991ff42
5kyBuilder <124388396+5kyBuilder@users.noreply.github.com>
2024-01-06 12:13:08 +0300

    Add files via upload

--- Project 164.py
+++ Project 164.py
@@ -11,20 +11,26 @@ root.configure(bg="grey70")
 
 Image_Label = Label(root, image="")
 
-name = ""
+file1 = ""
 
 def openFile():
-    global name
+    global file1
     file1 = filedialog.askopenfilename(title="Open Text File", filetypes=[("Image Files", "*.png *.jpg *.jpeg *.gif")])
     print(file1)
     Image_Imported = ImageTk.PhotoImage(Image.open(file1))
     Image_Label.configure(image=Image_Imported)
-    Image_Label["image"] = Image_Imported
-    file1 = open(file1, "r")
-    file1.close()
+    Image_Label.image = Image_Imported
+    
+def rotateImage():
+    ImageFound = Image.open(file1)
+    rotatedImage = ImageFound.rotate(180)
+    Image_Imported = ImageTk.PhotoImage(rotatedImage)
+    
+    Image_Label.configure(image=Image_Imported)
+    Image_Label.image = Image_Imported
     
 Open_Button = Button(root, command=openFile, text="Open Image")
-Rotate_Button = Button(root, text="Rotate Image")
+Rotate_Button = Button(root, command=rotateImage, text="Rotate Image")
 
 Open_Button.pack()
 Image_Label.pack()
